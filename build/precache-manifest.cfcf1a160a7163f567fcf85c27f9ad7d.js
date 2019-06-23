aa8ed534f25da90db7423af11bb052ce426e04f2
DejanVega <d.besic@vegaitsourcing.rs>
2019-06-23 16:07:19 +0200

    Adding builded version

---
+++ build/precache-manifest.cfcf1a160a7163f567fcf85c27f9ad7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "47bff36c37660445f533",
+    "url": "/static/js/content.js"
+  },
+  {
+    "revision": "7826c86fdff804f2635d",
+    "url": "/static/js/app.js"
+  },
+  {
+    "revision": "101efd1b2ac64b75794a",
+    "url": "/static/js/0.chunk.js"
+  },
+  {
+    "revision": "47bff36c37660445f533",
+    "url": "/static/css/content.css"
+  },
+  {
+    "revision": "7826c86fdff804f2635d",
+    "url": "/static/css/app.css"
+  },
+  {
+    "revision": "bf1cd0a155d23fcf52cd6ecc747e55aa",
+    "url": "/index.html"
+  }
+];
